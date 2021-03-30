dcf49e98bd6f5001e6d82330d4bc9d9ded09903c
SavenkoVadym <savenko.vadym@gmail.com>
2021-03-30 15:26:44 +0300

    1.0

---
+++ 3D game by Pepega/Assets/Scripts/ItemInteractable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteractable : Interactable
+{
+	public override float StoppingDistance
+	{
+		get { return _interactionDistance * 0.1f; }
+	}
+
+	protected override void Interact()
+	{
+		base.Interact();
+		Debug.Log("Ты не можешь поднять " + transform.name);
+	}
+}
---
+++ 3D game by Pepega/Assets/Scripts/NPCInteractable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class NPCInteractable : Interactable
+{
+	protected override void Interact()
+	{
+		base.Interact();
+		Debug.Log(transform.name + " взаимодействует");
+	}
+}
